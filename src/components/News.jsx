ff99adcc8e69b0e95a59e249108bf91353d9483c
SJKed <simonjurstedtskolan@gmail.com>
2022-06-08 10:57:59 +0200

    angry meoji

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import '../App.css';
 
 
 function News() {
-    const token = "AAAAAAAAAAAAAAAAAAAAALjMdQEAAAAA4W09iav1RxWbqapsKhbL0Al6rrU%3DWur6GkBuFKtGFmpjDX0mn90uxTN4yzKPOfDRgz4D4ewPRogYpl";
-    const URL = "https://api.twitter.com/2/tweets/search/recent?query=%23iths2022"
-    const [Tweets, setTweets] = useState('Loading...');
+    // const token = "AAAAAAAAAAAAAAAAAAAAALjMdQEAAAAA4W09iav1RxWbqapsKhbL0Al6rrU%3DWur6GkBuFKtGFmpjDX0mn90uxTN4yzKPOfDRgz4D4ewPRogYpl";
+    // const URL = "https://api.twitter.com/2/tweets/search/recent?query=%23iths2022"
+    const [Tweets] = useState('Loading...');
 
     return (
         <div className="Widget News">
