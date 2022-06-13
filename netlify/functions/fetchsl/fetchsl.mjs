1d32069cdfe37d7deada92cc73d938c5513d6a8a
SJKed <simonjurstedtskolan@gmail.com>
2022-06-13 16:10:42 +0200

    fetchsl successfully functional

--- netlify/functions/fetchsl/fetchsl.mjs
+++ netlify/functions/fetchsl/fetchsl.mjs
@@ -1,26 +1,28 @@
 // for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
-
-const fetch = require('node-fetch')
+import fetch from "node-fetch"
 const token = "875bc083-478d-4124-8d6b-fff7a15ab122"
 
-const handler = async function (event, context) {
+exports.handler = async function (event, context) {
   try {
-    const response = await fetch(`https://api.resrobot.se/v2.1/departureBoard?id=740004046&format=json&accessId=${KEY}`, {
+    const response = await fetch(`https://api.resrobot.se/v2.1/departureBoard?id=740004046&format=json&accessId=${token}`, {
+      method: "GET",
       headers: {
         "Access-Control-Allow-Origin": "*"
       }
     });
-    const data = await response.text();
-    console.log(data)
-    
+    const data = await response.json();
+    console.log(data);
+
     return {
       statusCode: 200,
       body: JSON.stringify(data)
     };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify((err.message))
+    }
   }
-  catch (err) {
-    return { error: err.message }
-  }
+
 }
 
-module.exports = { handler }
