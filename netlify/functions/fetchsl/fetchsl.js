25985f9214bf81d84bc3b4dae125ffd9fee203a6
SJKed <simonjurstedtskolan@gmail.com>
2022-06-13 15:42:38 +0200

    added sl function and jsx functionality

---
+++ netlify/functions/fetchsl/fetchsl.js
@@ -0,0 +1,23 @@
+// for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
+
+const fetch = require('node-fetch')
+const token = "875bc083-478d-4124-8d6b-fff7a15ab122"
+
+const handler = async function (event, context) {
+  try {
+    fetch(`https://api.resrobot.se/v2.1/departureBoard?id=740004046&format=json&accessId=${KEY}`, {
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+  }
+  catch (err) {
+    return { error: err.message }
+  }
+}
+
+module.exports = { handler }
--- src/components/Transit.jsx
+++ src/components/Transit.jsx
@@ -1,6 +1,22 @@
+import { useEffect } from 'react';
 import '../App.css';
 
 function Transit() {
+
+    async function fetchSL() {
+        fetch('https://charming-pegasus-c43878.netlify.app/.netlify/functions/fetchsl')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+            })
+            .catch(error => console.log(error));
+    }
+
+    useEffect(() => {
+        setInterval(() => {
+            fetchSL();
+        }, 10000);
+    }, []);
     return (
         <div className="Widget Transit">
             <p>
@@ -9,5 +25,6 @@ function Transit() {
         </div>
     )
 }
+
 
 export default Transit;
