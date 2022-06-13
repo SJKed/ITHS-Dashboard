dc8e389b90e558bb46119d8d5766fe2634b3e64d
SJKed <simonjurstedtskolan@gmail.com>
2022-06-13 15:57:39 +0200

    refactor fetchsl.mjs

--- netlify/functions/fetchsl/fetchsl.mjs
+++ netlify/functions/fetchsl/fetchsl.mjs
@@ -5,15 +5,18 @@ const token = "875bc083-478d-4124-8d6b-fff7a15ab122"
 
 const handler = async function (event, context) {
   try {
-    fetch(`https://api.resrobot.se/v2.1/departureBoard?id=740004046&format=json&accessId=${KEY}`, {
+    const response = await fetch(`https://api.resrobot.se/v2.1/departureBoard?id=740004046&format=json&accessId=${KEY}`, {
       headers: {
         "Access-Control-Allow-Origin": "*"
       }
-    })
-      .then(res => res.text())
-      .then(data => {
-        console.log(data)
-      })
+    });
+    const data = await response.text();
+    console.log(data)
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
   }
   catch (err) {
     return { error: err.message }
