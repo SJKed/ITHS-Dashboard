8d4ee51800bee11a7c53a0ec0eee5fbb38053024
SJKed <simonjurstedtskolan@gmail.com>
2022-06-16 02:58:03 +0200

    reddit preview now works

--- netlify/functions/speedtest/speedtest.mjs
+++ netlify/functions/speedtest/speedtest.mjs
@@ -1,55 +1,49 @@
-// import FastSpeedTest from "fast-speedtest-api";
+// import fetch from "node-fetch";
 
-// exports.handler = async (event, context) => {
+// exports.cock = async function (event, context) {
 //   try {
-//     let speedtest = new FastSpeedTest({
-//       token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm", // required
-//       verbose: false, // default: false
-//       timeout: 10000, // default: 5000
-//       https: true, // default: true
-//       urlCount: 5, // default: 5
-//       bufferSize: 8, // default: 8
-//       unit: FastSpeedTest.UNITS.Mbps // default: Bps
-//     });
-
-//     speedtest.getSpeed().then(s => {
-//       console.log(`Speed: ${s} Mbps`);
-//       return {
-//         statusCode: 200,
-//         body: s
-//       }
-//     }).catch(e => {
-//       console.error(e.message);
-//       return {
-//         statusCode: 500,
-//         body: JSON.stringify(e.message)
+//     const response = fetch(`https://www.reddit.com/r/yugioh/top.json?limit=1`, {
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
 //       }
 //     });
-//   }
-//   catch (error) {
+//     const data = await response.json();
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(data)
+//     }
+//   } catch (err) {
 //     return {
 //       statusCode: 500,
-//       body: JSON.stringify(error.message)
+//       body: JSON.stringify(err.message)
 //     }
 //   }
-
 // }
 
 import fetch from "node-fetch";
+const URL = "https://www.reddit.com/r/yugioh/top.json?limit=5"
 
-exports.handlers = async function (event, context) {
+exports.handler = async (event, context) => {
   try {
-    const response = fetch(`https://www.reddit.com/r/yugioh/top.json?limit=1`);
+    const response = await fetch(URL);
     const data = await response.json();
+
+    const posts = data.data.children.map(instance => { 
+      const title = instance.data.title
+      const subreddit = instance.data.subreddit
+      let selftext = ""
+      if (instance.data.selftext) {
+        selftext = instance.data.selftext
+      }
+      return { title, subreddit, selftext }
+    })
     return {
       statusCode: 200,
-      body: JSON.stringify(data)
-    }
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify(err.message)
-    }
+      body: JSON.stringify(posts)
+    };
+  }
+  catch (err) {
+    return { error: err.message };
   }
 }
 
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,15 +1,18 @@
 import '../App.css';
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 function Timeline() {
+
+    const [posts, setPosts] = useState([{ title: "Loading...", subreddit: "Loading...", selftext: "Loading..."}]);
 
     async function fetchSpeed() {
         fetch('https://charming-pegasus-c43878.netlify.app/.netlify/functions/speedtest')
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-            }
-            )
+                setPosts(data);
+                console.log(posts)
+            })
             .catch(error => console.log(error));
     }
 
@@ -19,7 +22,15 @@ function Timeline() {
 
     return (
         <div className="Widget Timeline">
-            <p>Hola hombre</p>
+            {posts.map((post, index) => {
+                return (
+                    <div className="Post" key={index}>
+                        <h3>{post.title}</h3>
+                        <p>{post.selftext}</p>
+                        <p>{post.subreddit}</p>
+                    </div>
+                );
+            })}
         </div>
     );
 }
